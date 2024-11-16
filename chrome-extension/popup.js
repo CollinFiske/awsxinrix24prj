baf3aff441fd2c5af22ebaf6e1a252e12e91fa16
Franco Garcia <116780712+francoeGarcia@users.noreply.github.com>
2024-11-16 15:47:50 -0800

    Update popup.js

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,35 +1,11 @@
-document.getElementById("fetchData").addEventListener("click", () => {
-  fetch("http://localhost:5000/api/data")
-    .then(response => response.json())
-    .then(data => {
-      document.getElementById("response").textContent = data.message;
-    })
-    .catch(error => {
-      console.error("Error fetching data:", error);
-    });
-});
+let userInput = "";
 
 document.getElementById("submitButton").addEventListener("click", () => {
-  const userInput = document.getElementById("userInput").value;
+  userInput = document.getElementById("userInput").value;
 
-  if (userInput) {
-    // send input to flask 
-    fetch('http://localhost:5000/save-input', {
-      method: 'POST', headers: {
-        'Content-type' : 'aplication/json'
-      },
-      body: JSON.stringify({ input: userInput})
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log("Input saved: ", data)
-    })
-    .catch(error => {
-      console.error("Error saving input:", error);
-    });
-  } else {
-    console.log("No input provided.");
-  }
+  console.log("User Input ", userInput);
+
+  
 });
 
 document.getElementById("extract").addEventListener("click", () => {
