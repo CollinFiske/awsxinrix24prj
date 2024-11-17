3f057822bdb283fbac2464cd52486009860a56e1
Franco Garcia <116780712+francoeGarcia@users.noreply.github.com>
2024-11-16 16:36:49 -0800

    Update popup.js

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -10,21 +10,3 @@ document.addEventListener("DOMContentLoaded", function() {
     inputField.value = "";
   });
 });
-
-document.getElementById("extract").addEventListener("click", () => {
-  console.log("Extracting??")
-  // Inject and run the content script in the active tab
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    if (tabs.length === 0) {
-      console.error("No active tab found!");
-      return;
-    }
-
-    chrome.scripting.executeScript({
-      target: { tabId: tabs[0].id },
-      files: ["content.js"]
-    }, () => {
-      console.log("Content script injected successfully.");
-    });
-  });
-});
